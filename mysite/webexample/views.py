0a1c368646021a89c8800403adda4e2ab05401e1
Alex New <zorro.789@yandex.ru>
2019-11-11 21:49:25 +0300

    git add .

--- mysite/webexample/views.py
+++ mysite/webexample/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse ("<h3>Привет мир!</h3>")
+	return HttpResponse ("<h3>Привет мир!!!</h3>")
